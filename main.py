4418b735c15c3d2d424896285f1d11bbe45fbc0a
Anthony M <amendo309@gmail.com>
2023-04-25 16:46:00 -0500

    Refactor code from functions to class object.

--- main.py
+++ main.py
@@ -2,58 +2,55 @@
 import requests, csv, api_key, reader, os
 from bs4 import BeautifulSoup
 
-#set variable for api token, pull real token from .gitignored api_key file
-access_token = api_key.token
 
-# global values being used
-artist = []
-ids = []
-albums_ids = []
-albums_name_list = []
-albums_release_date = []
-artist_name_list = []
-tracklist_urls = []
-artist_id_term = None
-primary_artist_id = None
-primary_artist_name = None
-search_term = None
+class GeniusScraper():
+	def __init__(self):
+		self.access_token = api_key.token
+		self.artist = []
+		self.ids = []
+		self.albums_ids = []
+		self.albums_name_list = []
+		self.albums_release_date = []
+		self.artists_name_list = []
+		self.tracklist_urls = []
+		self.artist_id_term = None
+		self.primary_artist_id = None
+		self.primary_artist_name = None
+		self.search_term = None
 
-class fileSetup():
-	def fileOpener():
+	def file_opener(self):
 		file = open('csv.csv')
 		fileReader = csv.DictReader(file, ['name'])
 
 		for row in fileReader:
-			artist.append(row['name'])
+			self.artist.append(row['name'])
 
-	def artistID(list):
+	def artist_ID(self, artist):
 		print('artistID: Getting api url and artist IDs...')
-		for x in list:
-			search_term = x
-			genius_search_url = f"http://api.genius.com/search?q={search_term}&access_token={token}"
-
-			print('Genius Search Url: ' + genius_search_url)
+		for artists in artist:
+			self.search_term = artists
+			genius_search_url = f"http://api.genius.com/search?q={self.search_term}&access_token={self.access_token}"
 			response = requests.get(genius_search_url)
 			json_data = response.json()
 
 			for hit in json_data['response']['hits']:
-				if hit['result']['primary_artist']['name'] == search_term:
-					primary_artist_id = hit['result']['primary_artist']['id']
-					ids.append(primary_artist_id)
-					primary_artist_name = hit['result']['primary_artist']['name']
-					artist_name_list.append(primary_artist_name)
+				if hit['result']['primary_artist']['name'] == self.search_term:
+					self.primary_artist_id = hit['result']['primary_artist']['id']
+					self.ids.append(self.primary_artist_id)
+					self.primary_artist_name = hit['result']['primary_artist']['name']
+					self.artists_name_list.append(self.primary_artist_name)
 
-					print('Primary Artist ID: ' + primary_artist_id)
-					print('IDS: ' + ids)
-					print('Primary Artist ID: ' + primary_artist_name)
-					print('Artist Name List: ' + artist_name_list)
+					# print('Primary Artist ID: ' + self.primary_artist_id)
+					# print('IDS: ' + self.ids)
+					# print('Primary Artist ID: ' + self.primary_artist_name)
+					# print('Artist Name List: ' + self.artist_name_list)
 					break
 
-	def albumList(list):
+	def album_list(self, ids):
 		print('albumList: Getting public api url and album lists...')
-		for y in list:
-			artist_id_term = y
-			public_url_albums = f"http://genius.com/api/artists/{artist_id_term}/albums?per_page=50/"
+		for artist_id in ids:
+			self.artist_id_term = artist_id
+			public_url_albums = f"http://genius.com/api/artists/{self.artist_id_term}/albums?per_page=50/"
 
 			print('Public URL Albums: ' + public_url_albums)
 			response2 = requests.get(public_url_albums)
@@ -62,24 +59,24 @@ class fileSetup():
 			for albums in album_json_data['response']['albums']:
 				if albums['_type'] == "album":
 					album_id = albums['api_path']
-					albums_ids.append(album_id)
+					self.albums_ids.append(album_id)
 					albums_name = albums['name']
-					albums_name_list.append(albums_name)
+					self.albums_name_list.append(albums_name)
 					albums_date = albums['release_date_components']
-					albums_release_date.append(albums_date)
+					self.albums_release_date.append(albums_date)
 				else:
 					break
 
-	def songList(list):
+	def song_list(self, albums_ids):
 			print('songList: Getting public api url for each specific album and tracklist...')
-			for z in list:
-				album_id_term = z
+			for album_id in albums_ids:
+				album_id_term = album_id
 				public_url_tracklist = f"http://genius.com/api{album_id_term}/tracks"
-				tracklist_urls.append(public_url_tracklist)
+				self.tracklist_urls.append(public_url_tracklist)
 
-	def createArtistFolder(list, nlist):
-		for h in list:
-			artist_name_term = str(h)
+	def createArtistFolder(self, artists_name_list, albums_name_list):
+		for artist_name in artists_name_list:
+			artist_name_term = str(artist_name)
 			parent_dir = "/Users/amendo/repos/lyrics/files"
 			path = os.path.join(parent_dir, artist_name_term)
 
@@ -88,8 +85,8 @@ class fileSetup():
 			except OSError as error:
 				print(error)
 
-		for g in nlist:
-			album_name_term = str(g)
+		for album_name in albums_name_list:
+			album_name_term = str(album_name)
 			parent_dir2 = f"/Users/amendo/repos/lyrics/files/{artist_name_term}"
 			path2 = os.path.join(parent_dir2, album_name_term)
 
@@ -98,5 +95,14 @@ class fileSetup():
 			except OSError as error:
 				print(error)
 
+	def scrape(self):
+		self.file_opener()
+		self.artist_ID(self.artist)
+		self.album_list(self.ids)
+		self.song_list(self.albums_ids)
+		self.createArtistFolder(self.artists_name_list, self.albums_name_list)
+
+scraper = GeniusScraper()
+scraper.scrape()
 
 
