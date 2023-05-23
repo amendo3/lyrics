5a5a9181c92e8948c3e0312c142ceaa167b94f31
Anthony M <amendo309@gmail.com>
2023-05-23 18:28:31 -0500

    Got most of the statements working well again. Some print messages for error checking. About to test new method of tying songs to Album class instances.

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class artistReader():
 				artist_name = row[0]
 				artist = Artist(artist_name)
 				self.artists.append(artist)
-				print(f"artistReader - Read artist: {artist_name}")
+				# print(f"artistReader - Read artist: {artist_name}")
 
 		return self.artists
 
@@ -58,7 +58,7 @@ class artistIdScraper():
 					primary_artist_id = hit['result']['primary_artist']['id']
 
 					artist.ids = primary_artist_id
-					print(f"artistIdScraper - Scraped artist ID for '{id_search_term}': {primary_artist_id}")
+					# print(f"artistIdScraper - Scraped artist ID for '{id_search_term}': {primary_artist_id}")
 					break
 
 class albumIdScraper():
@@ -67,8 +67,8 @@ class albumIdScraper():
 		self.albums = []
 
 
-		def scraper(self):
-			for artist in self.artists_to_get_album_ids:
+	def scraper(self):
+		for artist in self.artists_to_get_album_ids:
 			artist_id_term = artist.ids
 			public_url_albums = f"http://genius.com/api/artists/{artist_id_term}/albums?per_page=50/"
 
@@ -83,44 +83,45 @@ class albumIdScraper():
 
 					album = Album(artist, album_name, album_id, album_date)
 					self.albums.append(album)
-					print(f"albumIdScraper - Scraped album ID for '{album_name}': {album_id}")
+					# print(f"albumIdScraper - Scraped album ID for '{album_name}': {album_id}")
 
 
 				else:
 					print('ALBUM LIST ERROR')
 					break
 
-class songListScraper():
-	def __init__(self, albums):
-		self.albums = albums
+# class songListScraper():
+# 	def __init__(self, albums):
+# 		self.albums = albums
 
-		self.songs_list = []
+# 		self.songs_list = []
 
-		def scraper(self):
-			for album in self.albums:
-			song_id_term = album.ids
-			public_url_tracklist = f"http://genius.com/api{song_id_term}/tracks"
+# 	def scraper(self):
+# 		for album in self.albums:
+# 			song_id_term = album.ids
+# 			public_url_tracklist = f"http://genius.com/api{song_id_term}/tracks"
 
-			print(public_url_tracklist)
+# 			# print(public_url_tracklist)
 
-			response = requests.get(public_url_tracklist)
-			tracklist_json_data = response.json()
-
-
-			for tracks in tracklist_json_data['response']['tracks']:
-				if tracks['song']['_type'] == "song" and tracks['song']['lyrics_state'] == "complete":
-					songs_id = tracks['song']['api_path']
-					full_title = tracks['song']['full_title']
-					songs_date = tracks['song']['release_date_with_abbreviated_month_for_display']
+# 			response = requests.get(public_url_tracklist)
+# 			tracklist_json_data = response.json()
 
 
-					songs = Songs(album.artist, full_title, songs_id, songs_date)
-					self.songs_list.append(songs)
+# 			for tracks in tracklist_json_data['response']['tracks']:
+# 				if tracks['song']['_type'] == "song" and tracks['song']['lyrics_state'] == "complete":
+# 					songs_id = tracks['song']['api_path']
+# 					full_title = tracks['song']['full_title']
+# 					songs_date = tracks['song']['release_date_with_abbreviated_month_for_display']
 
-				else:
-					error_url = f"http://genius.com/api{song_id_term}/tracks"
-					print(f"SONG LIST ERROR - {error_url}")
-					continue
+
+# 					songs = Songs(album.artist, full_title, songs_id, songs_date)
+# 					self.songs_list.append(songs)
+# 					# print(f"songListScraper - Scraped song: {full_title}")
+
+# 				else:
+# 					error_url = f"http://genius.com/api{song_id_term}/tracks"
+# 					# print(f"SONG LIST ERROR - {error_url}")
+# 					continue
 
 class createFolders():
 	def __init__(self, artists):
@@ -135,7 +136,7 @@ class createFolders():
 
 			try:
 				os.mkdir(artist_dir)
-				print(f"createFolders - Created folder: {artist_dir}")
+				# print(f"createFolders - Created folder: {artist_dir}")
 			except OSError as error:
 				print(error)
 
@@ -149,29 +150,54 @@ class logic():
 
 		self.album_id_scraper = albumIdScraper(self.artists)
 
-		# self.song_list_scraper = None
 
 
+	def get_artist_ids(self):
+		self.artist_id_scraper.scraper()
 
-		def get_artist_ids(self):
-			self.artist_id_scraper.scraper()
+	def get_album_ids(self):
+		self.album_id_scraper.scraper()
 
-		def get_album_ids(self):
-			self.album_id_scraper.scraper()
+	# def get_song_list(self):
+	# 	self.song_list_scraper = songListScraper(self.album_id_scraper.albums)
+	# 	self.song_list_scraper.scraper()
 
-		def get_song_list(self):
-			self.song_list_scraper = songListScraper(self.album_id_scraper.albums)
-			self.song_list_scraper.scraper()
-
-		def create_folders(self):
-			folder_creator = createFolders(self.artists)
-			folder_creator.artistFolder()
+	def create_folders(self):
+		folder_creator = createFolders(self.artists)
+		folder_creator.artistFolder()
 
 run = logic()
 run.get_artist_ids()
 run.get_album_ids()
-run.get_song_list()
+# run.get_song_list()
 run.create_folders()
+
+
+# Print Artists
+print("Artists:")
+for artist in run.artist_id_scraper.artists_to_scrape:
+    print(f"Artist Name: {artist.name}")
+    print(f"Artist ID: {artist.ids}")
+    print()
+
+# Print Albums
+print("Albums:")
+for album in run.album_id_scraper.albums:
+    print(f"Artist Name: {album.artist.name}")
+    print(f"Album Name: {album.name}")
+    print(f"Album ID: {album.ids}")
+    print(f"Release Date: {album.date}")
+    print()
+
+# Print Songs
+# print("Songs:")
+# for song in run.song_list_scraper.songs_list:
+#     print(f"Artist Name: {song.artist.name}")
+#     print(f"Song Name: {song.name}")
+#     print(f"Song ID: {song.ids}")
+#     print(f"Release Date: {song.date}")
+#     print(f"Lyrics: {song.lyrics}")
+#     print()
 
 
 
